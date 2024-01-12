7f7bb98b2e9fc2d10b28cea3746f2063f268b28d
cria <victor55cria@gmail.com>
2024-01-11 22:08:25 -0300

    pre-register automatic

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,32 +1,38 @@
 /// <reference types="cypress" />
 
+
 context('functionality Login', () => {
 
-    it('he mus log in successfully', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('he mus log in successfully', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('a > .hidden-xs').should('contain','Welcome aluno_ebac !')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain','A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
+        cy.get('a > .hidden-xs').should('contain', 'Welcome aluno_ebac !')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
     })
 
     it('it should show an error message : user incorrect ', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@test.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
 
     })
     it('it should show an error message : password incorrect ', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
-        
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+
     })
- 
+
 
 })
---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+import { fa, faker } from '@faker-js/faker';
+
+
+describe('pre-register', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+    it('He must to complete pre-register successfully', () => {
+
+       var senha = faker.internet.password() 
+       var senha2 = faker.internet.password() 
+
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(senha)
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('#password_current').type(senha)
+        cy.get('#password_1').type(senha2)
+        cy.get('#password_2').type(senha2)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
+    });
+});
