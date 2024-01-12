9270dbf45358c627238e564198fcef97ae71d1a3
cria <victor55cria@gmail.com>
2024-01-11 21:18:08 -0300

    automatic login test

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+context('functionality Login', () => {
+
+    it('he mus log in successfully', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('a > .hidden-xs').should('contain','Welcome aluno_ebac !')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain','A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
+    })
+
+    it('it should show an error message : user incorret ', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@test.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+
+    })
+    it('it should show an error message : password incorret ', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+        
+    })
+ 
+
+})
