e560fe0bc948f2a7d619fe8802dea471263238dd
cria <victor55cria@gmail.com>
2024-01-17 20:52:00 -0300

    commit shopping ebac

---
+++ cypress/e2e/produtos-ebac.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+context('functionality  make shopping', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('he must add shopping in the shopping cart', () => {
+
+        var quantidade = 20
+
+        cy.get('[class="product-block grid"]').contains('Aether Gym Pant').click()
+        cy.get('.button-variable-item-36').click().click()
+        cy.get('.button-variable-item-Brown').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain',(quantidade))
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aether Gym Pant” foram adicionados no seu carrinho.')
+
+
+        
+    });
+});
